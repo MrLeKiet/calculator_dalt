9d20a3206ff9c56816bc96e7bbb5d12280d78768
MrLeKiet <kietsvis@gmail.com>
2024-11-26 15:34:04 +0700

    Bug

--- lib/calculator_layout.dart
+++ lib/calculator_layout.dart
@@ -74,6 +74,8 @@ class _CalculatorLayoutState extends State<CalculatorLayout> {
     }
   }
 
+  //Random comment
+
   bool isButtonEnabled(String buttonText) {
     if (widget.isProgrammer) {
       if (widget.currentMode == 'HEX') {
